3f89fcdf5e31d151480030cb90dbdc67d5a7dd6f
Eslam-Elngar <eslamelngar555@gmail.com>
2026-03-26 22:28:28 +0200

    nw projext added

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,6 +1,18 @@
 export const Projects = [
   {
     Id: 0,
+    Title: "Reelax",
+    SubTitle: "A movie discovery platform with ratings, trailers, and search — built with Next.js, TypeScript, and TMDb API",
+    Description:
+      "Reelax is a modern Arabic movie platform where users can discover trending, popular, and top-rated films, watch trailers, browse by categories, rate movies with an interactive star system, and save favorites locally. Features include a cinematic hero slider, cast details, reviews, full search functionality, and a fully responsive dark-themed design. Powered by the TMDb API.",
+    Category: "Web App",
+    Techs: ["Next js", "Tailwind", "typeScript"],
+    Img: "/Projects-img/eelax.png",
+    Visit: "https://Reelax-movies-platform.vercel.app/",
+    Code: "https://github.com/eslamelngar-dev/Reelax-Movies-Platform",
+  },
+  {
+    Id: 1,
     Title: "Shogol",
     SubTitle: "A Front-End freelance platform built with Next.js, TypeScript",
     Description:
@@ -11,8 +23,8 @@ export const Projects = [
     Visit: "https://shogol-freelancers-platform.vercel.app/",
     Code: "https://github.com/eslamelngar-dev/Shogol-Freelancers-Platform",
   },
-{
-    Id: 1,
+  {
+    Id: 2,
     Title: "AskAway",
     SubTitle: "A Front-End AI ChatBot Interface built with Next.js, TypeScript",
     Description:
@@ -21,21 +33,9 @@ export const Projects = [
     Techs: ["Next js", "Tailwind", "typeScript"],
     Img: "/Projects-img/askaway.png",
     Visit: "https://ask-away-ai-chat-bot-interface.vercel.app/",
-},
-  {
-    Id: 2,
-    Title: "Soum Teck",
-    SubTitle: "Real-Time Auction Platform — Front-End Web Application",
-    Description:
-      "A Front-End auctions website built with a real-time bidding logic. Features include countdown-based auctions. Designed to highlight practical implementation of modern web technologies and clean system structure.",
-    Category: "Web App",
-    Techs: ["Next js", "Tailwind", "typeScript"],
-    Img: "/Projects-img/soum-teck.png",
-    Visit: "https://soum-teck-auctions-web-application.vercel.app/",
-    Code: "https://github.com/eslamelngar-dev/SoumTeck-auctions-webApplication",
   },
-  {
-    Id: 3,
+{
+    Id: 4,
     Title: "Fitness Zone",
     SubTitle:
       "Special pages for egyptian healthy food, All types of exersices for every part of your body.",
@@ -47,4 +47,17 @@ export const Projects = [
     Visit: "https://fitness-zone-workouts-for-everyone.vercel.app/",
     Code: "https://github.com/eslamelngar-dev/Fitness-Zone---Workouts-for-everyone",
   },
+  {
+    Id: 3,
+    Title: "Soum Teck",
+    SubTitle: "Real-Time Auction Platform — Front-End Web Application",
+    Description:
+      "A Front-End auctions website built with a real-time bidding logic. Features include countdown-based auctions. Designed to highlight practical implementation of modern web technologies and clean system structure.",
+    Category: "Web App",
+    Techs: ["Next js", "Tailwind", "typeScript"],
+    Img: "/Projects-img/soum-teck.png",
+    Visit: "https://soum-teck-auctions-web-application.vercel.app/",
+    Code: "https://github.com/eslamelngar-dev/SoumTeck-auctions-webApplication",
+  },
+  
 ];
